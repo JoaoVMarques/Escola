f0b8256336f5f8e952e3bbdb5c789fd07362bca5
ooyy Vdk <j.v.marques.fernandes@gmail.com>
2025-01-21 18:25:58 -0300

    Criando a rota de login

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Home from './pages/Home'
+import { BrowserRouter } from 'react-router'
+import Router from './pages/Routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   </StrictMode>,
 )
