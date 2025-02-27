d89ee85485a0bae67f5a7b3e4bfa698e5ad5faa1
JoaoMarques <j.v.marques.fernandes@gmail.com>
2025-02-27 03:23:36 -0300

    Adicionando Formik como dependencia e criando um objeto com o forumlario de login

--- front-end/src/pages/Login/components/LoginFormContainer.tsx
+++ front-end/src/pages/Login/components/LoginFormContainer.tsx
@@ -1,37 +1,66 @@
+import { Formik } from 'formik';
 import { Col, Container, Form, Row } from 'react-bootstrap';
+import * as yup from 'yup';
 
 function LoginFormContainer() {
+  const schema = yup.object().shape({
+    email: yup.string().required(),
+    password: yup.string().required(),
+  });
 
   return (
-    <Form className="mt-5">
-      <Container>
-        <Row>
-          <Col md={6}>
-            <h2 className="text-center">Entrar</h2>
-            <h6 className="text-center">acesse com seu e-mail e senha</h6>
-          </Col>
-        </Row>
-        <Row className="mb-3">
-          <Col md={6}>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Control type="email" placeholder="Email" />
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row className="mb-3">
-          <Col md={6}>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Control type="password" placeholder="Senha" />
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            <button type="submit" className="btn btn-primary w-100">Entrar</button>
-          </Col>
-        </Row>
-      </Container>
-    </Form>
+    <Formik
+      validationSchema={schema}
+      onSubmit={console.log}
+      initialValues={{
+        email: '',
+        password: '',
+      }}
+    >
+      {({ handleSubmit, handleChange, values }) => (
+        <Form className="mt-5" onSubmit={handleSubmit}>
+          <Container>
+            <Row>
+              <Col md={6}>
+                <h2 className="text-center">Entrar</h2>
+                <h6 className="text-center">acesse com seu e-mail e senha</h6>
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col md={6}>
+                <Form.Group>
+                  <Form.Control
+                    id="email"
+                    type="email" 
+                    placeholder="Email" 
+                    onChange={handleChange} 
+                    value={values.email} 
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col md={6}>
+                <Form.Group>
+                  <Form.Control
+                    id="password"
+                    type="password" 
+                    placeholder="Senha" 
+                    onChange={handleChange} 
+                    value={values.password}
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={6}>
+                <button type="submit" className="btn btn-primary w-100">Entrar</button>
+              </Col>
+            </Row>
+          </Container>
+        </Form>
+      )}
+    </Formik>
   );
 }
 
